b68849edc3b72aa496f287e689014a7cf85e616b
Markus Faßbender <markus.fassbender@posteo.de>
2024-07-30 20:52:48 +0200

    add flickr service to dependencies

--- PhotoHikes/PhotoHikes/AppDependency.swift
+++ PhotoHikes/PhotoHikes/AppDependency.swift
@@ -5,18 +5,28 @@
 //  Created by Markus on 26.07.24.
 //
 
+import NetworkService
+
 /// AppDependency follows the composition root design pattern. It's a structured way to share dependencies between scenes.
 ///
 /// See this post for further explanation: https://simonbs.dev/posts/introducing-the-composition-root-pattern-in-a-swift-codebase/
 actor AppDependency {
     
-    private var _locationService: (any LocationServiceProtocol)!
     
+    var flickrService: any FlickrServiceProtocol { _flickrService }
     var locationService: any LocationServiceProtocol { _locationService }
+    
+    private var _flickrService: (any FlickrServiceProtocol)!
+    private var _locationService: (any LocationServiceProtocol)!
+    private var _networkService: NetworkService!
     
     // MARK: - Set Up
     
     func setUp() async {
+        let networkService = NetworkService(urlSession: .shared)
+        _networkService = networkService
+        
+        _flickrService = FlickrService(networkService: networkService)
         _locationService = await LocationService()
     }
 }
